4ae8fe3038f6a1a284d57606a39bb29316f890e0
pbrunzema <paul.brunzema@dsme.rwth-aachen.de>
2026-05-31 11:41:01 -0400

    update gp

--- app.py
+++ app.py
@@ -34,9 +34,9 @@ SITE = {
             '<a href="https://unravel.rwth-aachen.de">UnRAVel Research Training Group</a>, funded by the DFG.'
         ),
         (   
-            "I am currently a research scientist intern at "
+            "Currently, I am a research scientist intern at "
             '<a href="https://meta.com">Meta</a> '
-            "working on Bayesian optimization and AutoML. "
+            "in New York, NY working on Bayesian optimization and AutoML. "
             "In summer 2025, I joined the "
             '<a href="https://www.tri.global/research/epic">EPIC group at Toyota Research Institute (TRI)</a> '
             "in Los Altos, CA as a research intern, working on autonomous racing in changing conditions."
